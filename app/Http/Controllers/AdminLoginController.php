0be60021c2d4ce160f5a0ccd1f9fb997667f3712
Iloba <scarlettsney@gmail.com>
2021-08-23 20:43:15 +0100

    commit

--- app/Http/Controllers/AdminLoginController.php
+++ app/Http/Controllers/AdminLoginController.php
@@ -3,11 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rules\Password;
 
 class AdminLoginController extends Controller
 {
     public function login(Request $request){
         
-        dd($request->all());
+        $request->validate([
+            'email' => 'required',
+            'password' => [
+                
+                        'required', 'confirmed',
+                        Password::min(8)->letters()->numbers()->mixedCase()
+                
+                    ],
+        ]);
     }
 }
--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -4,19 +4,19 @@
         <div class="container">
             <div class="col-md-6 mx-auto">
                 <div class="card p-4">
-                    @include('layouts.messages')
+                    
                     <h4 class="text-center mb-5">Login</h4>
                     
                     <form action="{{route('login_admin')}}" method="POST">
                         @csrf
-                        <div>
-                            <input type="text" name="email" class="form-control" placeholder="Email"> <br>
+                        <div class="mb-3">
+                            <input type="text" name="email" class="form-control" placeholder="Email"> 
                             @error('email')
                                <span class="text-danger text-small">{{$message}}</span>
                             @enderror
                         </div>
-                        <div>
-                            <input type="password" name="password" class="form-control mb-4" placeholder="Password">
+                        <div class="mb-3">
+                            <input type="password" name="password" class="form-control" placeholder="Password">
                             @error('password')
                             <span class="text-danger text-small">{{$message}}</span>
                             @enderror
