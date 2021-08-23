203d417c36070bd5d15f3b6272e9d255ba37ffd0
Iloba <scarlettsney@gmail.com>
2021-08-23 08:06:48 +0100

    commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="stylesheet" href="{{asset('icofont/icofont/icofont.min.css')}}">
+</head>
+<body>
+    <div class="bg-info p-4">
+       <p class="text-center text-light"> &copy; Kaduna Polytechnic 2021</p>
+    </div>
+</body>
+</html>
