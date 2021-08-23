6fe22c9555f33c118a56b8ffa75cfc7f2024fcc2
Iloba <scarlettsney@gmail.com>
2021-08-23 08:13:46 +0100

    commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,13 +25,11 @@
                 <div class="row">
                     <div class="col-md-10 mx-auto mt-5">
                         <div class="card p-3 shadow-sm">
-                           
-                                <h1  class="text-center text-uppercase text-secondary font-bold mt-3">Management Information system for computer science Staff</h1>
-                                <div class="mx-auto mt-3 mb-3">
-                                    <a class="btn btn-success mx-auto" href="{{route('register')}}">Register</a>
-                                    <a class="btn btn-info text-light" href="{{route('login')}}">Login</a>
-                                </div>
-                          
+                            <h1  class="text-center text-uppercase text-secondary font-bold mt-3">Management Information system for computer science Staff</h1>
+                            <div class="mx-auto mt-3 mb-3 ">
+                                <a class="btn btn-success mx-auto m-2" href="{{route('register')}}">Register</a>
+                                <a class="btn btn-info text-light m-2" href="{{route('login')}}">Login</a>
+                            </div>
                         </div>
                         <div class="card-footer bg-info">
                             <p class="text-light text-center mt-3">Project 2021</p>
